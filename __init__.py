e6c1f1a86e70b635fcd54bc109f568e0dba042fc
inori333 <202200130178@mail.sdu.edu.cn>
2026-03-18 18:54:39 +0800

    feat: wire all modules together in addon registration

--- __init__.py
+++ __init__.py
@@ -10,10 +10,31 @@ bl_info = {
     "category": "3D View",
 }
 
+try:
+    import bpy
+    _bpy_available = True
+except ImportError:
+    _bpy_available = False
+
+if _bpy_available:
+    from ui.preferences import PREFERENCE_CLASSES
+    from ui.operators import OPERATOR_CLASSES
+    from ui.chat_panel import PANEL_CLASSES
+
+    ALL_CLASSES = PREFERENCE_CLASSES + OPERATOR_CLASSES + PANEL_CLASSES
 
 def register():
-    pass
-
+    if not _bpy_available:
+        return
+    for cls in ALL_CLASSES:
+        bpy.utils.register_class(cls)
+    bpy.types.Scene.aimm_chat_input = bpy.props.StringProperty(name="消息", description="输入消息给 AI 助手", default="")
+    bpy.types.Scene.aimm_is_loading = bpy.props.BoolProperty(name="加载中", default=False)
 
 def unregister():
-    pass
+    if not _bpy_available:
+        return
+    del bpy.types.Scene.aimm_is_loading
+    del bpy.types.Scene.aimm_chat_input
+    for cls in reversed(ALL_CLASSES):
+        bpy.utils.unregister_class(cls)
